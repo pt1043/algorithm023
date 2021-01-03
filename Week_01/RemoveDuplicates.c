08166be7cfedee27d17f75b4fa2feda3e01ed048
pt1043 <pt1043@126.com>
2021-01-03 17:45:51 +0800

    Week01Assignment

--- Week_01/RemoveDuplicates.c
+++ Week_01/RemoveDuplicates.c
@@ -1,9 +1,9 @@
 int removeDuplicates (int *nums,int numsSize) {
-        if (numsSize == 0 || numsSize == 1) retrun numsSize;
+        if (numsSize == 0 || numsSize == 1) return numsSize;
         //k_is_Counter
         int k = 1;
 	for (int i = 1; i < numsSize; i++){
-		if (num[i] != nums[i-1]) {
+		if (nums[i] != nums[i-1]) {
 		nums[k++] = nums[i];
 		}
 	}
